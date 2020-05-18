dd727750eb1be88f90297d9548f3210ddbe7def2
Nexd <statser07@gmail.com>
2020-05-18 05:00:56 +0200

    Add files via upload

--- erettsegi/4_Tarsalgo/tarsalgo.pas
+++ erettsegi/4_Tarsalgo/tarsalgo.pas
@@ -1,168 +1,201 @@
-﻿//Emelt érettségi 2018 május, http://dload.oktatas.educatio.hu/erettsegi/feladatok_2018tavasz_emelt/e_inf_18maj_fl.pdf
-program tarsalgo;
-uses crt, strutils, sysutils;
+﻿program tarsalgo;
+uses crt;
 
 const max = 1000;
-	  adatok = 'ajto.txt';
-
-Type Sorok = record
+Type Adatok = record
 	Ora:integer;
 	Perc:integer;
-	Szemely:integer;
-	Irany:boolean;
+	Azon:integer;
+	Irany:string;
 END;
 
-var	fajl:text;
-	Lista:array[1..max] of Sorok;
+var fajl:text;
+	Lista:array[1..max] of Adatok;
 	ossz:integer = 0;
-
-Procedure Beolvasas();
-var sor:string;
-	darab:array[1..4] of string;
-	i:integer;
+	bentvan:array[1..100] of boolean;
+	
+Procedure Feladat1();
 BEGIN
-	Assign(fajl, adatok);
+	Assign(fajl, 'ajto.txt');
 	Reset(fajl);
-
+	
 	While NOT EOF(fajl) do BEGIN
-		ReadLn(fajl, sor);
-		for i := 1 to 4 do BEGIN
-			darab[i] := ExtractWord(i, sor, [' ']);
-		END;
-
 		ossz := ossz + 1;
-		Lista[ossz].Ora := StrToInt(darab[1]);
-		Lista[ossz].Perc := StrToInt(darab[2]);
-		Lista[ossz].Szemely := StrToInt(darab[3]);
-		if(darab[4] = 'be') then BEGIN
-			Lista[ossz].Irany := true;
-		END else BEGIN
-			Lista[ossz].Irany := false;
-		END;
+		ReadLn(fajl, Lista[ossz].Ora, Lista[ossz].Perc, Lista[ossz].Azon, Lista[ossz].Irany);
 	END;
-
+	
 	Close(fajl);
 END;
 
 Procedure Feladat2();
-var osszesen:integer;
+var i:integer;
+	utolso:integer = 0;
 BEGIN
-	WriteLn('2. feladat');
-
-	osszesen := ossz;
-	WriteLn('Az első belépő: ', Lista[1].Szemely);
-
-	While NOT(osszesen = 0) do BEGIN
-		if(Lista[osszesen].Irany = false) then BEGIN
-			WriteLn('Az utolsó kilépő: ', Lista[osszesen].Szemely);
-			break;
+	WriteLn('2. feladat:');
+	WriteLn('Az első belépő: ', Lista[1].Azon);
+	
+	for i := 1 to ossz do BEGIN
+		if(Lista[i].Irany = ' ki') then BEGIN
+			utolso := i;
 		END;
-
-		osszesen := osszesen - 1;
 	END;
+	
+	WriteLn('Az utolsó kilépő: ', Lista[utolso].Azon);
 END;
 
 Procedure Feladat3();
 var i:integer;
 	k:integer;
-	szemelyek:integer = 0;
-	haladas:array[1..max] of integer;
+	athaladas:array[1..100] of integer;
 BEGIN
-	for i := 1 to max do BEGIN
-		haladas[i] := 0;
+	for i := 1 to 100 do BEGIN
+		athaladas[i] := 0;
 	END;
-
-	for i := 1 to ossz do BEGIN
-		if(Lista[i].Szemely > szemelyek) then BEGIN
-			szemelyek := Lista[i].Szemely;
-		END;
-	END;
-
-	for i := 1 to szemelyek do BEGIN
+	
+	for i := 1 to 100 do BEGIN
 		for k := 1 to ossz do BEGIN
-			if(Lista[k].Szemely = i) then BEGIN
-				haladas[i] := haladas[i] + 1;
+			if(Lista[k].Azon = i) then BEGIN
+				athaladas[i] := athaladas[i] + 1;
 			END;
 		END;
 	END;
-
+	
 	Assign(fajl, 'athaladas.txt');
 	ReWrite(fajl);
 
-	for i := 1 to szemelyek do BEGIN
-		if(haladas[i] > 0) then BEGIN
-			WriteLn(fajl, i, ' ', haladas[i]);
+	for i := 1 to 100 do BEGIN
+		if(athaladas[i] > 0) then BEGIN
+			WriteLn(fajl, i, ' ', athaladas[i]);
 		END;
 	END;
-
+	
 	Close(fajl);
 END;
 
 Procedure Feladat4();
-var hol:array[1..max] of boolean;
-	i:integer;
+var i:integer;
 	k:integer;
-	szemelyek:integer = 0;
 BEGIN
-	WriteLn('4. feladat');
-
-	for i := 1 to ossz do BEGIN
-		if(Lista[i].Szemely > szemelyek) then BEGIN
-			szemelyek := Lista[i].Szemely;
-		END;
-	END;
-
-	for i := 1 to szemelyek do BEGIN
-		hol[i] := false;
-	END;
-
-	for i := 1 to szemelyek do BEGIN
+	WriteLn('4. feladat:');
+	Write('A végén a társalgóban voltak: '); //1 11 22 24 29 30 35 37
+	
+	for i := 1 to 100 do BEGIN
 		for k := 1 to ossz do BEGIN
-			if(Lista[k].Szemely = i) then BEGIN
-				hol[i] := Lista[k].Irany;
+			if(Lista[k].Azon = i) then BEGIN
+				Case Lista[k].Irany Of
+					' be':bentvan[i] := true;
+					' ki':bentvan[i] := false;
+				END;
 			END;
 		END;
 	END;
-
-	WriteLn('A végén a társalgóban voltak: ');
-	for i := 1 to szemelyek do BEGIN
-		if(hol[i] = true) then BEGIN
+	
+	for i := 1 to 100 do BEGIN
+		if(bentvan[i]) then BEGIN
 			Write(i, ' ');
 		END;
 	END;
+	
+	WriteLn;
 END;
 
 Procedure Feladat5();
 var i:integer;
 	letszam:integer = 0;
-	mikor:integer = 0;
-	maxletszam:integer = 0;
+	ora:integer = 0;
+	perc:integer = 0;
+	max:integer = 0;
 BEGIN
 	WriteLn('5. feladat:');
-
+	
 	for i := 1 to ossz do BEGIN
-		if(Lista[i].Irany = true) then BEGIN
-			letszam := letszam + 1;
-		END else BEGIN
-			letszam := letszam - 1;
+		Case Lista[i].Irany Of
+			' be':letszam := letszam + 1;
+			' ki':letszam := letszam - 1;
 		END;
-
-		if(letszam > maxletszam) then BEGIN
-			mikor := i;
+		
+		if(letszam > max) then BEGIN
+			max := letszam;
+			ora := Lista[i].Ora;
+			perc := Lista[i].Perc;
 		END;
 	END;
+	
+	WriteLn('Például ', ora, ':', perc, '-kor voltak a legtöbben a társalgóban.');
+END;
 
-	WriteLn('Például ', Lista[mikor].Ora, ':', Lista[mikor].Perc, '-kor voltak a legtöbben a társalgóban.');
+Procedure Feladat7(azonosito:integer) Forward;
+Procedure Feladat8(azonosito:integer) Forward;
+
+Procedure Feladat6();
+var azonosito:integer;
+BEGIN
+	WriteLn('6. feladat:');
+	Write('Adja meg a személy azonosítóját! ');
+	ReadLn(azonosito);
+	
+	Feladat7(azonosito);
+END;
+
+Procedure Feladat7(azonosito:integer);
+var i:integer;
+BEGIN
+	WriteLn;
+	WriteLn('7. feladat:');
+	for i := 1 to ossz do BEGIN
+		if(Lista[i].Azon = azonosito) then BEGIN
+			if(Lista[i].Irany = ' be') then BEGIN
+				Write(Lista[i].Ora, ':', Lista[i].Perc, '-');
+			END else BEGIN
+				Write(Lista[i].Ora, ':', Lista[i].Perc);
+				WriteLn;
+			END;
+		END;
+	END;
+	
+	WriteLn;
+	Feladat8(azonosito);
+END;
+
+Procedure Feladat8(azonosito:integer);
+var i:integer;
+	osszperc:integer = 0;
+	perc:integer;
+BEGIN
+	WriteLn;
+	WriteLn('8. feladat:');
+	
+	for i := 1 to ossz do BEGIN
+		if(Lista[i].Azon = azonosito) then BEGIN
+			if(Lista[i].Irany = ' be') then BEGIN
+				perc := Lista[i].Perc;
+			END else BEGIN
+				osszperc := osszperc + (Lista[i].Perc - perc);
+			END;
+			
+			WriteLn(osszperc);
+		END;
+	END;
+	
+	Write('A(z) ', azonosito, '. személy összesen ', osszperc, ' percet volt bent,');
+	Case bentvan[azonosito] Of
+		true: Write(' a megfigyelés végén a társalgóban volt.');
+		false: Write(' a megfigyelés végén nem volt a társalgóban.');
+	END;
 END;
 
 BEGIN
 	ClrScr;
-
-	Beolvasas();
+	
+	Feladat1();
 	Feladat2();
+	WriteLn;
 	Feladat3();
 	Feladat4();
+	WriteLn;
 	Feladat5();
+	WriteLn;
+	Feladat6();
 	
 	WriteLn;
 	WriteLn;
