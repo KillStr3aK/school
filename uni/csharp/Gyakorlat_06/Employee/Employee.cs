b3bc0a2c860b25acbfb6f3975187af6def9cbe2f
Nexd <statser07@gmail.com>
2021-03-18 11:39:32 +0100

    Add files via upload

---
+++ uni/csharp/Gyakorlat_06/Employee/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Employee
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public long Salary { get; private set; }
+
+        private static readonly int Retirement = 65;
+        private static readonly string[] Months = { "", "január", "február", "március", "április", "május", "június", "július", "augusztus", "szeptember", "október", "november", "december" };
+
+        public Employee(string name, int year, string month, int day)
+        {
+            Name = name;
+
+            int monthIndex = ConvertMonthName(month);
+            if (monthIndex == 0)
+                monthIndex = 1;
+
+            BirthDate = new DateTime(year, monthIndex, day);
+            Salary = 10000 * (DateTime.Now.Year - BirthDate.Year);
+        }
+
+        public Employee(string name, int year, int month, int day) : this(name, year, Months[month], day) { }
+
+        public int YearsLeft()
+        {
+            return Retirement - (DateTime.Now.Year - BirthDate.Year);
+        }
+
+        public string ConvertMonthValue(int month)
+        {
+            return Months[month];
+        }
+
+        public int ConvertMonthName(string name)
+        {
+            for(int i = 0; i < Months.Length; i++)
+            {
+                if (Months[i] == name)
+                    return i;
+            }
+
+            return 0;
+        }
+
+        public static Employee Younger(Employee e1, Employee e2)
+        {
+            return e1.YearsLeft() > e2.YearsLeft() ? e1 : e2;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {BirthDate.Year}. {ConvertMonthValue(BirthDate.Month)} {BirthDate.Day}. {Salary} {YearsLeft()}";
+        }
+    }
+}
---
+++ uni/csharp/Gyakorlat_06/Employee/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeeCount = ReadInt("Employee amount");
+            Employee[] employees = new Employee[employeeCount];
+
+            for(int i = 0; i < employees.Length; i++)
+            {
+                Console.Write(i + 1 + ". employee name: ");
+                string name = Console.ReadLine();
+
+                int year = ReadInt("Employee birth year: ");
+                int month = ReadInt("Employee birth month: ");
+                int day = ReadInt("Employee birth day: ");
+                employees[i] = new Employee(name, year, month, day);
+            }
+
+            PrintArray(employees);
+            Console.WriteLine("Retirement check");
+            RetirementCheck(employees);
+
+            Console.WriteLine("More than average");
+            MoreThanAverage(employees);
+
+            Console.WriteLine("Sort by ascending");
+            SortAscending(employees);
+            PrintArray(employees);
+
+            Console.WriteLine("Sort by descending");
+            SortDescending(employees);
+            PrintArray(employees);
+        }
+
+        static void PrintArray(Employee[] arr)
+        {
+            foreach(Employee i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void RetirementCheck(Employee[] arr)
+        {
+            foreach(Employee i in arr)
+            {
+                if (i.YearsLeft() < 5)
+                    Console.WriteLine(i);
+            }
+        }
+
+        static void MoreThanAverage(Employee[] arr)
+        {
+            int average = AverageRetirement(arr);
+            foreach (Employee i in arr)
+            {
+                if (i.YearsLeft() > average)
+                    Console.WriteLine(i);
+            }
+        }
+
+        static int AverageRetirement(Employee[] arr)
+        {
+            int average = 0;
+            foreach (Employee i in arr)
+            {
+                average += i.YearsLeft();
+            }
+
+            return average / arr.Length;
+        }
+
+        static void SortAscending(Employee[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int index = i;
+
+                for(int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[k].YearsLeft() < arr[index].YearsLeft())
+                        index = k;
+                }
+
+                if(index != i)
+                {
+                    Employee temp = arr[i];
+                    arr[i] = arr[index];
+                    arr[index] = temp;
+                }
+            }
+        }
+
+        static void SortDescending(Employee[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int index = i;
+
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[k].YearsLeft() > arr[index].YearsLeft())
+                        index = k;
+                }
+
+                if (index != i)
+                {
+                    Employee temp = arr[i];
+                    arr[i] = arr[index];
+                    arr[index] = temp;
+                }
+            }
+        }
+
+        static int ReadInt(string msg)
+        {
+            Console.WriteLine(msg);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
